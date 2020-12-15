fbd50f9efb3ed6aedc66412d450900ee72bccbe8
takumi <65800363+hatakumi323@users.noreply.github.com>
2020-12-15 16:38:37 +0900

    スクロールに合わせて背景色を変える

--- mian.js
+++ mian.js
@@ -1,7 +1,19 @@
-$('.about').waypoint({
-  handler(direction) {
-  
-  },
+//スクロール発火イベント
+window.addEventListener( "scroll" ,function(){
 
-  offset: '100%',
+  //スクロールの高さを取得
+  let scroll = window.pageYOffset;
+  
+  if( scroll > 4000 ){ 
+    document.body.style.backgroundColor = '#FFC400';
+  }else if( scroll > 3000 ){
+    document.body.style.backgroundColor = '#43A047';
+  }else if( scroll > 2000 ){
+    document.body.style.backgroundColor = '#FF6F00';
+  }else if( scroll > 1000 ){
+    document.body.style.backgroundColor = '#0091EA';
+  }else{
+    document.body.style.backgroundColor = '#FF4081';
+  }
+  
 });

250d5d86f0b65111b482b15ae321266fbfb72281
takumi <65800363+hatakumi323@users.noreply.github.com>
2021-01-16 10:49:52 +0900

    worksの追加

--- main.js
+++ main.js
@@ -33,3 +33,16 @@ const updateButton = () => {
   
   // ページの途中でリロード（再読み込み）された場合でも、ボタンが表示されるようにする
   updateButton();
+
+
+  // アンカーリンク
+  $(function(){
+    $('a[href^="#"]').click(function(){
+      var speed = 600;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+      $("html, body").animate({scrollTop:position}, speed, "swing");
+      return false;
+    });
+  });
